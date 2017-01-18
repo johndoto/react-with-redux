56f88c495cc1ec7ff1d0a0d34cef0b82d406fd2a
John Doto <johndoto@users.noreply.github.com>
2017-01-18 17:48:46 -0500

    finished adding redux-thunk and mock api

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,77 +2,26 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
 
   constructor(props, context) {
     super(props, context);
-    this.state = {
-      course: { title: "" }
-    };
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
-  }
-
-  onTitleChange(event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({ course: course });
-  }
-
-  onClickSave() {
-    this.props.actions.createCourse(this.state.course);
-  }
-
-  courseRow(course, index) {
-    return <li key={index}>{course.title}</li>;
   }
 
   render() {
+    const {courses} = this.props;
+
     return (
       <div className="container">
-
         <h1>Courses</h1>
-
-        <div className="row">
-          <div className="col-xs-12 col-sm-6">
-
-            <h2>Course List</h2>
-
-            <ol>
-              {this.props.courses.map(this.courseRow)}
-            </ol>
-
-          </div>
-          <div className="col-xs-12 col-sm-6">
-
-            <h2>Add Course</h2>
-
-            <div className="form-group">
-              <input
-                type="text"
-                className="form-control"
-                onChange={this.onTitleChange}
-                value={this.state.course.title} />
-            </div>
-
-            <div className="form-group text-right">
-              <input
-                type="submit"
-                className="btn btn-primary"
-                value="Save"
-                onClick={this.onClickSave} />
-            </div>
-
-          </div>
-        </div>
-
+        <CourseList courses={courses}/>
       </div>
     );
   }
 
 }
-
 
 CoursesPage.propTypes = {
   courses: PropTypes.array.isRequired,
