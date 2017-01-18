5fe09433d637c3e111b7892a6b5987d46a970b59
John Doto <johndoto@users.noreply.github.com>
2017-01-18 12:32:32 -0500

    finished module on adding Redux

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,14 +1,94 @@
 import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends React.Component {
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      course: { title: "" }
+    };
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
+
+  onTitleChange(event) {
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({ course: course });
+  }
+
+  onClickSave() {
+    this.props.actions.createCourse(this.state.course);
+  }
+
+  courseRow(course, index) {
+    return <li key={index}>{course.title}</li>;
+  }
+
   render() {
     return (
       <div className="container">
+
         <h1>Courses</h1>
+
+        <div className="row">
+          <div className="col-xs-12 col-sm-6">
+
+            <h2>Course List</h2>
+
+            <ol>
+              {this.props.courses.map(this.courseRow)}
+            </ol>
+
+          </div>
+          <div className="col-xs-12 col-sm-6">
+
+            <h2>Add Course</h2>
+
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                onChange={this.onTitleChange}
+                value={this.state.course.title} />
+            </div>
+
+            <div className="form-group text-right">
+              <input
+                type="submit"
+                className="btn btn-primary"
+                value="Save"
+                onClick={this.onClickSave} />
+            </div>
+
+          </div>
+        </div>
 
       </div>
     );
   }
+
 }
 
-export default CoursesPage;
+
+CoursesPage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+  return {
+    courses: state.courses
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import 'babel-polyfill'; // can be reduced in size, by cherry-picking libraries
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 import routes from './routes';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './styles/styles.css'; // webpack can import CSS files!
 
+const store = configureStore();
+
 render(
-  <Router history={browserHistory} routes={routes} />,
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
   document.getElementById('app')
 );
